28e47cab91c5f716739aa8f8a84001d7ee4e0840
Hannah Rappaport <hannahgrappaport@gmail.com>
2024-04-11 14:38:47 -0400

    Initial Commit

---
+++ practiceApp/practiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceAppApp.swift
+//  practiceApp
+//
+//  Created by Hannah Rappaport on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct practiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
